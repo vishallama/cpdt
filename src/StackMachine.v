5d1f5de3e19bcc89422a21580d5fc6590e3f8d80
Vishal Lama <vishal.lama@gmail.com>
2017-04-30 08:51:03 -0600

    Add basic syntax for the source language

--- src/StackMachine.v
+++ src/StackMachine.v
@@ -3,3 +3,20 @@ Require Import CpdtTactics.
 
 Set Implicit Arguments.
 Set Asymmetric patterns.
+
+
+(* 2.1 - Arithmetic Expressions Over Natural Numbers *)
+
+Inductive binop : Set :=
+| Plus
+| Times.
+
+Inductive exp : Set :=
+| Const : nat -> exp
+| Binop : binop -> exp -> exp -> exp.
+
+Definition binopDenote (b : binop) : nat -> nat -> nat :=
+  match b with
+  | Plus => plus
+  | Times => mult
+  end.
