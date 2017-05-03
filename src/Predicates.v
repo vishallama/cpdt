17facac3b0f069d58387f1d175d355c2182e40a4
Vishal Lama <vishal.lama@gmail.com>
2017-05-03 08:51:28 -0600

    Add notes on constructive Propositional Logic

--- src/Predicates.v
+++ src/Predicates.v
@@ -2,3 +2,43 @@ Require Import List.
 Require Import CpdtTactics.
 
 Set Implicit Arguments.
+
+
+(* 4.1 - Propositional Logic *)
+Section Propositional.
+  Variables P Q R : Prop.
+
+  Theorem obvious : True.
+  Proof. apply I. Qed.
+
+  Theorem obvious' : True.
+  Proof. constructor. Qed.
+
+  Theorem False_imp : False -> 2 + 2 = 5.
+  Proof. destruct 1. Qed.
+
+  Theorem arith_neq : 2 + 2 = 5 -> 9 + 9 = 835.
+  Proof. intro. elimtype False. crush. Qed.
+
+  Theorem arith_neq' : ~ (2 + 2 = 5).
+  Proof. unfold not; crush. Qed.
+
+  Theorem and_comm : P /\ Q -> Q /\ P.
+  Proof. destruct 1; split; assumption. Qed.
+
+  Theorem or_comm : P \/ Q -> Q \/ P.
+  Proof. destruct 1; [right | left]; assumption. Qed.
+
+  Theorem or_comm' : P \/ Q -> Q \/ P.
+  Proof. tauto. Qed.
+
+  Theorem arith_comm : forall ls1 ls2 : list nat,
+    length ls1 = length ls2 \/ length ls1 + length ls2 = 6 ->
+    length (ls1 ++ ls2) = 6 \/ length ls1 = length ls2.
+  Proof. intuition. rewrite app_length; tauto. Qed.
+
+  Theorem arith_comm' : forall ls1 ls2 : list nat,
+    length ls1 = length ls2 \/ length ls1 + length ls2 = 6 ->
+    length (ls1 ++ ls2) = 6 \/ length ls1 = length ls2.
+  Proof. Hint Rewrite app_length. crush. Qed.
+End Propositional.
